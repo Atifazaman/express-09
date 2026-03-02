0972c7c3a69542d65d17e0568a0ee8ad96bc655a
Atifazaman <136952774+Atifazaman@users.noreply.github.com>
2026-03-02 19:05:03 +0530

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -1,13 +1,10 @@
 const express=require('express')
 const productRouter=express.Router()
 
-productRouter.get("/",(req,res)=>{
-    res.send(`<h1>Fetching all products</h1>`)
-})
-productRouter.get("/:id",(req,res)=>{
-res.send(`Fetching product with ID: ${req.params.id}`)
-})
-productRouter.post("/",(req,res)=>{
-res.send(`Adding a new product`)
-})
+const productController=require("../controllers/productController")
+
+productRouter.get("/",productController.getProduct)
+productRouter.get("/:id",productController.getProductById)
+productRouter.post("/",productController.postProduct)
+
 module.exports=productRouter
